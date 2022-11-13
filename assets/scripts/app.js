0c4e69e9a57b7f8d375bb6c248689195d2a75343
YevaMV <eva280988@gmail.com>
2022-11-14 00:13:32 +0400

    work with user input values

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,9 @@ const headerAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = document.querySelector('.btn--passive');
 const addMovieButton = document.querySelector('.btn--success');
+const userInputs = addMovieModal.querySelectorAll('input');
+
+const movies = [];
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
@@ -13,15 +16,48 @@ const toggleMovieHandler = () => {
   toggleBackdrop();
 };
 
+const clearMovieInput = () => {
+  for (const userInput of userInputs) {
+    userInput.value = '';
+  }
+};
+
 const cancelAddMovieHandler = () => {
   toggleMovieHandler();
+  clearMovieInput();
 };
 
 const backdropClickHandler = () => {
   toggleMovieHandler();
 };
 
-const addMovieHandler = () => {};
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Invalid input values (rating between 1 and 5).');
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieHandler();
+  clearMovieInput();
+};
 
 headerAddMovieButton.addEventListener('click', toggleMovieHandler);
 backdrop.addEventListener('click', backdropClickHandler);
